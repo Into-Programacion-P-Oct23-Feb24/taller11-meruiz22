877d9c04f5b2b809827fcbd22035601c1a6b718d
meruiz22 <meruiz22@utpl.edu.ec>
2024-01-04 17:24:13 -0500

    cambios

--- ejercicios/Problemas/src/problemas/Problema2.java
+++ ejercicios/Problemas/src/problemas/Problema2.java
@@ -22,11 +22,11 @@ public class Problema2 {
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
         int opcion;
-        System.out.println("Ingrese: 1 para calcular el area de un cuadrad\n "
+        System.out.println("Ingrese:\n 1 para calcular el area de un cuadrad\n "
                 + "2 para calcular el area de un triangulo\n "
                 + "3 para calcularc el area de un rectangulo");
         opcion = entrada.nextInt();
-        if (opcion == 1) {
+        if (opcion == 1){
             obtenerAreaCuadrado();
         } else if (opcion == 2) {
             obtenerAreaTriangulo();
@@ -41,18 +41,46 @@ public class Problema2 {
     }
 
     public static void obtenerAreaCuadrado() {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
         double l;
         double m;
         System.out.println("Ingrese el valor de los lados del cudrado");
         l = entrada.nextDouble();
+        m=l*l*l*l;
+        System.out.println(m);
+        
 
     }
 
     public static void obtenerAreaTriangulo() {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        double b;
+        double a;
+        double m;
+        System.out.println("Ingrese la altura del alturadel triangulo");
+        a = entrada.nextDouble();
+        System.out.println("Ingrese la base del alturadel triangulo");
+        b = entrada.nextDouble();
+        m =(b*a)/2;
+        System.out.println(m);
+        
 
     }
 
     public static void obtenerAreaRectangulo() {
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        double b;
+        double a;
+        double m;
+        System.out.println("Ingrese la altura del alturadel triangulo");
+        a = entrada.nextDouble();
+        System.out.println("Ingrese la base del alturadel triangulo");
+        b = entrada.nextDouble();
+        m =(b*a);
+        System.out.println(m);
 
     }
 
